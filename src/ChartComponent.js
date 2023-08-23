90c4ea7f58be9e763143608889ce567171a7e162
RedBoard <ott.thomas68@gmail.com>
2023-08-23 17:31:59 +0200

    (|) remove scroll chart and implement brush selection

--- src/ChartComponent.js
+++ src/ChartComponent.js
@@ -1,75 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
-import { Box, Paper } from '@mui/material';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, Legend, Brush } from 'recharts';
+import { Box, Paper, button } from '@mui/material';
 import CustomLegend from './CustomLegend';
 
-function ChartComponent({ data, visiblePoints }) {
+function ChartComponent({ data }) {
     const [activeTiers, setActiveTiers] = useState([
         "HERO", "VETERAN", "LEGEND", "GOAT", "RESEARCHER", "EXPLORER", "ADVENTURER"
     ]);
 
-    const [startIndex, setStartIndex] = useState(17 - 14);
-
-    const [isDragging, setIsDragging] = useState(false);
-    const [lastDragX, setLastDragX] = useState(null);
-
-    const handleWheel = (event) => {
-        const moveAmount = event.deltaX;
-        if (moveAmount > 0) {
-            if (startIndex + visiblePoints < data.length) {
-                setStartIndex(prevIndex => prevIndex + 1);
-            }
-        } else {
-            if (startIndex > 0) {
-                setStartIndex(prevIndex => prevIndex - 1);
-            }
-        }
-    }
-
-    const handleMouseDown = (event) => {
-        setIsDragging(true);
-        setLastDragX(event.clientX);
-    }
-
-    const handleMouseMove = (event) => {
-        if (isDragging) {
-            const deltaX = lastDragX - event.clientX;
-            const moveBy = Math.round(deltaX / 100);
-            if (moveBy !== 0) {
-                setStartIndex(prevIndex => Math.min(Math.max(0, prevIndex + moveBy), data.length - visiblePoints));
-                setLastDragX(event.clientX);
-            }
-        }
-    }
-
-    const handleMouseUp = () => {
-        setIsDragging(false);
-    }
-
-    useEffect(() => {
-        window.addEventListener('mouseup', handleMouseUp);
-        return () => {
-            window.removeEventListener('mouseup', handleMouseUp);
-        }
-    }, []);
-
-    useEffect(() => {
-        if (data.length > visiblePoints) {
-            setStartIndex(data.length - visiblePoints);
-        }
-    }, [data, visiblePoints]);
-
     return (
-        <Box mt={3} onWheel={handleWheel}>
+        <Box mt={3}>
             <Paper elevation={5} style={{ background: '#f5f5f5' }}>
                 <Box p={3}>
                     <CustomLegend activeTiers={activeTiers} setActiveTiers={setActiveTiers} />
-                    <Box mt={4} display="flex" justifyContent="center" onMouseDown={handleMouseDown}
-                        onMouseMove={handleMouseMove}>
+                    <Box mt={4} display="flex" justifyContent="center">
                         <LineChart
                             width={800}
                             height={400}
-                            data={data.slice(startIndex, startIndex + visiblePoints)}
+                            data={data}
                             margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
                         >
                             <XAxis dataKey="signed_at" />
@@ -83,6 +31,19 @@ function ChartComponent({ data, visiblePoints }) {
                             {activeTiers.includes("RESEARCHER") && <Line type="monotone" dataKey="RESEARCHER" stroke="#650d1b" />}
                             {activeTiers.includes("EXPLORER") && <Line type="monotone" dataKey="EXPLORER" stroke="#0d656e" />}
                             {activeTiers.includes("ADVENTURER") && <Line type="monotone" dataKey="ADVENTURER" stroke="#6e0d44" />}
+                            <Brush
+                                dataKey="signed_at"
+                                height={40}
+                                stroke="#d0d0d0"
+                                travellerWidth={24}
+                                gap={5}
+                                travellerProps={{
+                                    width: 20,
+                                    height: 20,
+                                    y: -5,
+                                }}
+                                fill="#f5f5f5"
+                            />
                         </LineChart>
                     </Box>
                 </Box>
