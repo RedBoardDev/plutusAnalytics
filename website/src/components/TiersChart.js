3308e31413dbab214c5dce1d310fddd1d9f7cd88
RedBoardDev <ott.thomas68@gmail.com>
2023-11-12 19:44:57 +0100

    (+) adapte TierChart with barLabels from new API data

--- website/src/components/TiersChart.js
+++ website/src/components/TiersChart.js
@@ -20,37 +20,45 @@ const CustomRectangle = (props) => {
 };
 
 function TiersChart({ data }) {
-    console.log("dd", data);
     const colors = [
         "#1f77b4", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728",
         "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#aaffc3"
     ];
     const lastBlockHeight = data ? data[data.length - 1]?.block_height : 0;
     const lastData = data?.filter(item => item.block_height === lastBlockHeight) || [];
-    console.log("lastData", lastData)
     const activeTiers = {};
     lastData.forEach(element => {
         const tierName = element?.tier_name || undefined;
-        const tierValue = element?.tier_value || '0';
+        const tierDescription = element?.description || undefined;
 
         if (!activeTiers[tierName]) {
             activeTiers[tierName] = {};
         }
-        activeTiers[tierName][tierValue] = parseInt(element.value);
+        activeTiers[tierName][tierDescription] = parseInt(element.value);
     });
+
+    let barLabels = [];
+
     const barData = Object.keys(activeTiers).map((tierName, index) => {
         const tierValues = activeTiers[tierName];
         const barEntry = { name: tierName };
         let total = 0;
+
         Object.keys(tierValues).forEach((tierValue, idx) => {
-            barEntry[`DA 0`] = tierValues[tierValue];
-            total += tierValues[tierValue];
+            const tierName2 = Object.keys(tierValues)[idx];
+
+            if (!barLabels.includes(tierName2)) {
+                barLabels.push(tierName2);
+            }
+
+            barEntry[tierName2] = tierValues[tierName2];
+            total += tierValues[tierName2];
         });
         barEntry['total'] = total;
         return barEntry;
     });
+
     const sortedBarData = barData.sort((a, b) => a.total - b.total);
-    console.log(sortedBarData);
 
     return (
         <Box p={3}>
@@ -58,9 +66,6 @@ function TiersChart({ data }) {
                 <Box p={3}>
                     <Typography variant='h6' align="center" gutterBottom>
                         Staking per dynamic ajustement
-                    </Typography>
-                    <Typography variant='h8' align="center" gutterBottom>
-                        This graphic will be update with current tier and old tiers difference soon
                     </Typography>
                     <Box mt={4} display="flex" justifyContent="center">
                         <BarChart
@@ -73,10 +78,10 @@ function TiersChart({ data }) {
                             <YAxis />
                             <Tooltip />
                             <Legend />
-                            {Object.keys(activeTiers).map((tierName, index) => (
+                            {barLabels.map((tierName, index) => (
                                 <Bar
                                     key={tierName}
-                                    dataKey={`DA ${index}`}
+                                    dataKey={tierName}
                                     stackId="a"
                                     fill={colors[index % colors.length]}
                                     shape={<CustomRectangle />}
